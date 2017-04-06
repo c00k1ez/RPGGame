9924a436a52bbe053341bdb74a3af6e60d6b3e9f
CrAzZy_As <egorplotnikov@mail.ru>
2017-04-06 23:36:18 +0300

    Fuck the TypeScript!!!! Start to make game scene.

--- Client/JavaScript/Game.js
+++ Client/JavaScript/Game.js
@@ -1,19 +1,28 @@
 var MainGame = (function () {
     function MainGame() {
-        this.game = new Phaser.Game(1280, 600, Phaser.AUTO, "", { preload: this.preload, create: this.create, update: this.update });
+        this.HEIGHT = 600;
+        this.WIDTH = 1280;
+        this.game = new Phaser.Game(this.WIDTH, this.HEIGHT, Phaser.AUTO, "", { preload: this.preload, create: this.create, update: this.update });
     }
     MainGame.prototype.preload = function () {
-        this.game.load.spritesheet("Button", "./Assets/GUI/Button.png", 416, 416);
+        this.game.load.spritesheet("Button", "./Assets/GUI/Button.png", 400, 400);
+        this.game.load.image("Background", "./Assets/GUI/Background/back_stage.gif");
+        this.game.load.image("Backstage", "./Assets/GUI/Backstage/grass.png");
     };
     MainGame.prototype.create = function () {
-        var button = this.game.add.button(this.game.world.centerX - 208, 300, "Button", this.actionOnClick, this, 2, 1, 0);
+        this.background = this.game.add.tileSprite(0, 0, 1280, 600, "Background");
+        this.button = this.game.add.button(this.game.world.centerX - 208, 300, "Button", onClickEvent, this, 2, 1, 0);
     };
     MainGame.prototype.update = function () {
     };
-    MainGame.prototype.actionOnClick = function () {
-    };
     return MainGame;
 }());
+function onClickEvent() {
+    this.game.add.image(0, 0, "Backstage");
+    this.game.add.image(this.Backstage.WIDTH, 0, "Backstage");
+    this.button.visible = false;
+    this.background.visible = false;
+}
 window.onload = function () {
     var game = new MainGame();
 };
--- Client/TypeScript/Game.ts
+++ Client/TypeScript/Game.ts
@@ -3,30 +3,43 @@
 
 class MainGame {
 
-    game : Phaser.Game;
+    private game : Phaser.Game;
+
+    private background : Phaser.TileSprite;
+
+    private button : Phaser.Button;
+
+    private HEIGHT = 600;
+    private WIDTH = 1280;
 
     constructor() {
-        this.game = new Phaser.Game (1280, 600, Phaser.AUTO, "", { preload: this.preload, create: this.create, update: this.update });
+        this.game = new Phaser.Game (this.WIDTH, this.HEIGHT, Phaser.AUTO, "", { preload: this.preload, create: this.create, update: this.update });
     }
 
     preload() {
-        this.game.load.spritesheet("Button", "./Assets/GUI/Button.png", 416, 416);
+        this.game.load.spritesheet("Button", "./Assets/GUI/Button.png", 400, 400);
+        this.game.load.image("Background", "./Assets/GUI/Background/back_stage.gif");
+        this.game.load.image("Backstage", "./Assets/GUI/Backstage/grass.png");
     }
 
     create() {
-        var button = this.game.add.button(this.game.world.centerX - 208, 300, "Button", this.actionOnClick, this, 2, 1, 0);
-        
+        this.background = this.game.add.tileSprite(0, 0, 1280, 600, "Background");
+        this.button = this.game.add.button(this.game.world.centerX - 208, 300, "Button", onClickEvent, this, 2, 1, 0 );
     }
+
+   
 
     update() {
 
     }
-
-    actionOnClick() {
-
-    }
-
 }
+
+function onClickEvent() {
+        this.game.add.image(0, 0, "Backstage");
+        this.game.add.image(this.Backstage.WIDTH, 0, "Backstage");
+        this.button.visible = false;
+        this.background.visible = false;
+    }
 
 window.onload = () => {
 
